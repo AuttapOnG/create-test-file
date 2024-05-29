cdcea43e297d2e13f2abfe4d614568bb0962b1cc
AUTTAPONG TURA <auttapong.tura@scbtechx.io>
2024-05-29 16:54:57 +0700

    Update more menu

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,9 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import createTestFile from './createTestFile';
+import * as vscode from "vscode";
+import createTestFile from "./createTestFile";
+import openTestFile from "./openTestFile";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,13 +11,23 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('extension.createTestFileFromMenu', (fileUri?: vscode.Uri) => {
-    createTestFile(fileUri.fsPath);
-  });
+  const disposableCreateTestFileFromMenu = vscode.commands.registerCommand(
+    "extension.createTestFileFromMenu",
+    (fileUri?: vscode.Uri) => {
+      createTestFile(fileUri.fsPath);
+    }
+  );
 
-  context.subscriptions.push(disposable);
+  const disposableOpenTestFileFromMenu = vscode.commands.registerCommand(
+    "extension.openTestFileFromMenu",
+    (fileUri?: vscode.Uri) => {
+      openTestFile(fileUri.fsPath);
+    }
+  );
+
+  context.subscriptions.push(disposableCreateTestFileFromMenu);
+  context.subscriptions.push(disposableOpenTestFileFromMenu);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
